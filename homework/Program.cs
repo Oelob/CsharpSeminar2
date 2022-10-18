970f3f5238c01563fa020fa93d98420c42b169cb
Ivan <ab09@inbox.ru>
2022-10-18 23:38:49 +0500

    Решил две задчи

---
+++ homework/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+// // Задача 10 На входе трехзначное число, на выходе вторая цифра этого числа (рещение 1)
+// Console.Clear();
+// Console.WriteLine("Введите трехзначное число: ");
+// string num = Convert.ToString(Console.ReadLine());
+
+// Console.Write(num[1]);
+
+// // Задача 10 На входе трехзначное число, на выходе вторая цифра этого числа (рещение 2)
+
+// Console.Clear();
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (100 <= num  && num < 1000)
+// {
+//     Console.Write((num / 10) % 10); 
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число");
+// }    
+
+// Задача 13 Вывод третьей цифры заданного числа
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num < 1000)
+{
+    Console.Write(num % 10);
+}
+else if (num < 100)
+{
+    Console.Write("Третьей цифры в данном числе нет");
+}
+else 
+{
+    Console.Write("Число больше трехзначного");
+}
